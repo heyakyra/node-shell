fbd1ae0a8fd8c0d6a3e8639b2d14b57b0a544781
Kẏra <kxra@riseup.net>
2018-03-06 13:02:32 -0500

    added cat

--- bash.js
+++ bash.js
@@ -1,17 +1,24 @@
-const exportsFromPwd = require('./pwd');
-const ls = require('./ls');
+const pwd = require("./pwd");
+const ls = require("./ls");
+const cat = require("./cat");
 process.stdout.write("prompt > ");
 
 process.stdin.on("data", data => {
-  const cmd = data.toString().trim();
+  const input = data
+    .toString()
+    .trim()
+    .split(" ");
+  const cmd = input[0];
+  const args = input.slice(1);
 
   if (cmd === "pwd") {
-    exportsFromPwd();
+    pwd(args);
   } else if (cmd === "ls") {
-    ls();
+    ls(args);
+  } else if (cmd === "cat") {
+    cat(args);
   } else {
     process.stdout.write("You typed: " + cmd);
   }
-
   process.stdout.write("\nprompt > ");
 });
---
+++ cat.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+module.exports = function(args) {
+  fs.readFile(args[0], "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+};
